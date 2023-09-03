96f4edcef215992fe268fcf1db20266b55934632
Michael <mike.p.doc.71@gmail.com>
2023-09-02 19:47:52 -0500

    added test for region_dimension_graph

---
+++ tests/testthat/test_region_dimension_graph.R
@@ -0,0 +1,22 @@
+regions = c("Minnesota Eastern", "Minnesota Western")
+dimension_type = "width"
+p <- region_dimension_graph(c("Minnesota Eastern", "Minnesota Western"),
+                            dimension_type = "width")
+
+test_that("created plot exists", {
+  expect_is(p$layers[[1]], "ggproto")
+  expect_is(p$scales, "ggproto")
+})
+
+test_that("check labels", {
+  expect_identical(p$labels$x, "Drainage Area (sq miles)")
+  expect_identical(p$labels$y, "Width (feet)")
+})
+
+test_that("check data", {
+  expect_identical(unique(p$data$region_name),
+                   c("Minnesota Eastern", "Minnesota Western"))
+  expect_identical(unique(p$data$dimension), "width")
+  expect_equal(p$data$drainage_area, c(0.6, 1100.0, 7.5, 13500))
+  expect_equal(p$data$value, c(5.260561, 158.703230, 12.625824, 229.125596))
+})
